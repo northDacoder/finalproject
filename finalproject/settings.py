8b5c2afec2108779caa2a63ec89762548318de61
John Ryan <jcryan85@gmail.com>
2014-03-06 15:34:29 -0800

    update to settings.py file for initial apps added

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'developer_app',
+    'company_app',
 )
 
 MIDDLEWARE_CLASSES = (
