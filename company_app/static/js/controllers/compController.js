f23724cf93564e2a4e4ce090123c77f7fd7beaef
John Ryan <jcryan85@gmail.com>
2014-03-11 09:24:44 -0700

    Added admin for models

--- company_app/static/js/controllers/compController.js
+++ company_app/static/js/controllers/compController.js
@@ -52,3 +52,34 @@ app.config(function($scopeProvider){
 /**
  * Created by northDacoder on 3/10/14.
  */
+
+
+//app.controller('compController', function($scope, tastypieService) {
+//    var companyService = new tastypieService({
+//        apiUrl : '/api/v1/article/',
+//        fk : {user : '/api/v1/user/'},
+//        m2m : {comments : '/api/v1/comment/'},
+//        editRelated : ['comments'],
+//        keepFull : ['user']
+//    });
+//});
+
+
+// INITIALIZE TASTYPIE
+//<script>
+//var someService = new tastypieService({
+//    apiUrl : '<your API url here>',
+//    fk : {
+//        fk_field1 : '<foreign_key_field1_url>',
+//        fk_field2 : '<foreign_key_field2_url>',
+//        fk_field3 : '<foreign_key_field3_url>' // declare foreign key fields here
+//    },
+//    m2m : {
+//        m2m_field1 : '<m2m_key_field1_url>',
+//        m2m_field2 : '<m2m_key_field2_url>',
+//        m2m_field3 : '<m2m_key_field3_url>' // declare m2m fields here
+//    },
+//    editRelated : [], // list fields that needs to be saved together (implements save_related in backend)
+//    keepFull : [] // list fields that you don't want to save but still want to keep full (make sure you have full=True in your resource class)
+//});
+//</script>
--- developer_app/static/js/controllers/devController.js
+++ developer_app/static/js/controllers/devController.js
@@ -41,16 +41,6 @@ app.config(function($scopeProvider){
 		.when
 			{
 				controller: 'devController',
-				template: 'base.html'
-			}
-		.when
-			{
-				controller: 'devController',
-				template: 'home.html'
-			}
-		.when
-			{
-				controller: 'devController',
 				template: 'edit_developer.html'
 			}
 		.when
@@ -65,3 +55,34 @@ app.config(function($scopeProvider){
 			}
 		.otherwise({ redirectsTo: '/' });
 });
+
+
+
+//app.controller('devController', function($scope, tastypieService) {
+//    var developerService = new tastypieService({
+//        apiUrl : '/api/v1/article/',
+//        fk : {user : '/api/v1/user/'},
+//        m2m : {comments : '/api/v1/comment/'},
+//        editRelated : ['comments'],
+//        keepFull : ['user']
+//    });
+//});
+
+// INITIALIZE TASTYPIE
+//<script>
+//var someService = new tastypieService({
+//    apiUrl : '<your API url here>',
+//    fk : {
+//        fk_field1 : '<foreign_key_field1_url>',
+//        fk_field2 : '<foreign_key_field2_url>',
+//        fk_field3 : '<foreign_key_field3_url>' // declare foreign key fields here
+//    },
+//    m2m : {
+//        m2m_field1 : '<m2m_key_field1_url>',
+//        m2m_field2 : '<m2m_key_field2_url>',
+//        m2m_field3 : '<m2m_key_field3_url>' // declare m2m fields here
+//    },
+//    editRelated : [], // list fields that needs to be saved together (implements save_related in backend)
+//    keepFull : [] // list fields that you don't want to save but still want to keep full (make sure you have full=True in your resource class)
+//});
+//</script>
