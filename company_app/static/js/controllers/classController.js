776a00dbdd620e407538ef09999c819f4d24afeb
John Ryan <jcryan85@gmail.com>
2014-03-11 16:42:23 -0700

    added views & templates for both apps

--- company_app/static/js/controllers/classController.js
+++ company_app/static/js/controllers/classController.js
@@ -1,5 +1,19 @@
-function classControllerController($scope, $http, RocketSpace) {
+function classController($scope, $http, $routeParams) {
+      $http.get('api/v1/class/' + $routeParams.id + '?format=json').success(function(data){
 
-    console.log(RocketSpace.angular_instructor);
-    console.log("classController");
-}
+          console.log(data);
+          $scope.class = data;
+          $scope.students = data.students;
+          $scope.title = students.title;
+
+//          var classobj = data.objects;
+//          console.log(classobj);
+//          for (var i = 0; i < classobj.length; i++) {
+//              console.log(classobj[i]);
+//              cohorts.push(classobj[i]);
+//          }
+
+      }).error(function(data){
+          console.log("You have an error in your code");
+      });
+};
--- company_app/static/js/controllers/indexController.js
+++ company_app/static/js/controllers/indexController.js
@@ -2,4 +2,17 @@ function indexController($scope, $http, RocketSpace) {
 
     console.log(RocketSpace.angular_instructor);
     console.log("indexController");
+
+    $scope.classes = [];
+
+    $http.get('api/v1/student/?format=json').success(function(data){
+        console.log(data.objects);
+        $scope.students = data.objects;
+        RocketSpace.student_list = $scope.students;
+    });
+
+    $http.get('api/v1/class/?format=json').success(function(data){
+        console.log(data.objects);
+        $scope.classes = data.objects;
+    });
 }
