71327832772c406b3cdd18e10bd1339568eeed5a
John Ryan <jcryan85@gmail.com>
2014-03-11 23:05:40 -0700

    changes to devcontroller.js

--- developer_app/static/js/controllers/developerController.js
+++ developer_app/static/js/controllers/developerController.js
@@ -7,7 +7,7 @@ function developerController($scope, $http) {
     });
 
 
-    $scope.user = {'klass': "api/v1/class/1/", 'projects':[]};
+    $scope.user = {'klass': "api/v1/company/1/", 'projects':[]};
 
     $scope.addStudent = function() {
         console.log($scope.user);
@@ -18,7 +18,7 @@ function developerController($scope, $http) {
 
     $scope.deleteStudent = function() {
         console.log($scope.user);
-        $http:delete('api/v1/student/', $scope.user).success(function(){
+        $http:delete('api/v1/developer/', $scope.user).success(function(){
             alert('You are trying to delete a student!');
         });
     }
