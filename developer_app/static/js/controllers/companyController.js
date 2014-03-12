a6a7792dab5708f6bd96fff0ab6e566a4bd56bed
John Ryan <jcryan85@gmail.com>
2014-03-11 23:08:19 -0700

    changes to companyController.js file

--- developer_app/static/js/controllers/companyController.js
+++ developer_app/static/js/controllers/companyController.js
@@ -1,5 +1,5 @@
 function companyController($scope, $http) {
-    $http.get('api/v1/student/?format=json').success(function(data){
+    $http.get('api/v1/company/?format=json').success(function(data){
         console.log(data);
 
     }).error(function(data){
@@ -7,12 +7,12 @@ function companyController($scope, $http) {
     });
 
 
-    $scope.user = {'klass': "api/v1/class/1/", 'projects':[]};
+    $scope.user = {'klass': "api/v1/company/1/", 'projects':[]};
 
     $scope.addStudent = function() {
         console.log($scope.user);
-        $http:post('api/v1/student/', $scope.user).success(function(){
-            alert('Add a student?');
+        $http:post('api/v1/company/', $scope.user).success(function(){
+            alert('Add a Company?');
         });
     }
 
