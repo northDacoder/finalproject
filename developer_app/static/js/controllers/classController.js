8d8160abc075c0f87ce35ed5302730733122e97b
John Ryan <jcryan85@gmail.com>
2014-03-12 00:39:54 -0700

    added css styles

--- developer_app/static/js/controllers/classController.js
+++ developer_app/static/js/controllers/classController.js
@@ -6,14 +6,9 @@ function classController($scope, $http, $routeParams) {
           $scope.students = data.students;
           $scope.title = students.title;
 
-//          var classobj = data.objects;
-//          console.log(classobj);
-//          for (var i = 0; i < classobj.length; i++) {
-//              console.log(classobj[i]);
-//              cohorts.push(classobj[i]);
-//          }
+      }
 
       }).error(function(data){
           console.log("You have an error in your code");
       });
-};
+}
