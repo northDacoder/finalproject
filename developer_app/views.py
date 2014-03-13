97b5eb151734eb1a72c2d1e006cb454d3731e4a8
John Ryan <jcryan85@gmail.com>
2014-03-13 01:20:56 -0700

    changes to urls.py

--- developer_app/views.py
+++ developer_app/views.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from developer_app.forms import DeveloperForm, SignupForm, LoginForm
-from developer_app.models import Developer
-
+from developer_app.forms import SignupForm, LoginForm
 
 def signup(request):
     if request.method == "POST":
@@ -38,7 +36,7 @@ def login1(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect("developer_home.html")
+                    return redirect("angular.html")
 
     else:
         form1 = LoginForm()
@@ -51,44 +49,44 @@ def angular(request):
 def home(request):
     return render(request, "home.html")
 
-def developers(request):
-    developers = Developer.objects.all()
-    data = {'developers': developers}
-    return render(request, "developers/developers.html", data)
-
-
-def new_developer(request):
-    if request.method == "POST":
-        form = DeveloperForm(request.POST)
-        if form.is_valid():
-            if form.save():
-                return redirect("/developers")
-    else:
-        form = DeveloperForm()
-    data = {'form': form}
-    return render(request, "developers/new_developer.html", data)
-
-
-def view_developer(request, developer_id):
-    developer = Developer.objects.get(id=developer_id)
-    data = {"developer": developer}
-    return render(request, "developers/view_developer.html", data)
-
-
-def edit_developer(request, developer_id):
-    developer = Developer.objects.get(id=developer_id)
-    if request.method == "POST":
-        form = DeveloperForm(request.POST, instance=developer)
-        if form.is_valid():
-            if form.save():
-                return redirect("/developer/{}".format(developer_id))
-    else:
-        form = DeveloperForm(instance=developer)
-    data = {"developer": developer, "form": form}
-    return render(request, "developers/edit_developer.html",data)
-
-
-def delete_developer(request, developer_id):
-    developer = Developer.objects.get(id=developer_id)
-    developer.delete()
-    return redirect("/developers")
+# def developers(request):
+#     developers = Developer.objects.all()
+#     data = {'developers': developers}
+#     return render(request, "developers/developers.html", data)
+#
+#
+# def new_developer(request):
+#     if request.method == "POST":
+#         form = DeveloperForm(request.POST)
+#         if form.is_valid():
+#             if form.save():
+#                 return redirect("/developers")
+#     else:
+#         form = DeveloperForm()
+#     data = {'form': form}
+#     return render(request, "developers/new_developer.html", data)
+#
+#
+# def view_developer(request, developer_id):
+#     developer = Developer.objects.get(id=developer_id)
+#     data = {"developer": developer}
+#     return render(request, "developers/view_developer.html", data)
+#
+#
+# def edit_developer(request, developer_id):
+#     developer = Developer.objects.get(id=developer_id)
+#     if request.method == "POST":
+#         form = DeveloperForm(request.POST, instance=developer)
+#         if form.is_valid():
+#             if form.save():
+#                 return redirect("/developer/{}".format(developer_id))
+#     else:
+#         form = DeveloperForm(instance=developer)
+#     data = {"developer": developer, "form": form}
+#     return render(request, "developers/edit_developer.html",data)
+#
+#
+# def delete_developer(request, developer_id):
+#     developer = Developer.objects.get(id=developer_id)
+#     developer.delete()
+#     return redirect("/developers")
