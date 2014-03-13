67ffe7b0f681b2e328d9fbf9b1eef1065fd6e03a
John Ryan <jcryan85@gmail.com>
2014-03-12 18:10:01 -0700

    added registration forms

--- developer_app/views.py
+++ developer_app/views.py
@@ -5,6 +5,43 @@ from developer_app.forms import DeveloperForm
 from developer_app.models import Developer
 
 
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                form.cleaned_data["username"],
+                form.cleaned_data["email"],
+                form.cleaned_data["password"],
+            )
+        return redirect("login")
+
+    else:
+        form = SignupForm()
+    data = {'form': form}
+    return render(request, "signup.html", data)
+
+
+@login_required
+def special_page(request):
+    data = {}
+    return render(request, "charities/charities.html", data)
+
+def login1(request):
+    if request.method == "POST":
+        form1 = LoginForm(request.POST)
+        if form1.is_valid():
+            user = authenticate(username=form1.cleaned_data['username'], password=form1.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("charity_home.html")
+
+    else:
+        form1 = LoginForm()
+    data = {'form1': form1}
+    return render(request, "login.html", data)
+
 def angular(request):
     return render(request, "angular.html")
 
