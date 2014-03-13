7a59c5bee5f0d378eae853800a69a4701602402c
John Ryan <jcryan85@gmail.com>
2014-03-13 14:59:19 -0700

    added registration package

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -19,18 +19,24 @@ urlpatterns = patterns('',
     url(r'^api/', include(v1_api.urls)),
 
     url(r'^$', 'developer_app.views.angular', name="angular"),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+
+
     url(r'^register/developer/$', RegistrationView.as_view(success_url='/developer/new/'), name='registration_register_developer',),
     url(r'^register/company/$', RegistrationView.as_view(success_url='/company/new/'), name='registration_register_company',),
     url(r'^register/user/$', RegistrationView.as_view(success_url='/user/home/'), name='registration_register_developer',),
+
     url(r'^accounts/password/change/$', auth_views.password_change, name='password_change'),
     url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
+
     url(r'^accounts/password/reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^accounts/password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'', include('registration.backends.default.urls')),
+
+    #and now add the registration urls
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
 
 
