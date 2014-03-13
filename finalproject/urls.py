c6c5c55bc68fa4655916cdb15ae3c9eaa6687c6d
John Ryan <jcryan85@gmail.com>
2014-03-12 22:57:48 -0700

    added urls to urls.py

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -22,20 +22,20 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^developers/$', 'developer_app.views.developers', name='developers'),
-    url(r'^developers/new/$', 'developer_app.views.new_developer', name='new_developer'),
-    url(r'^developers/(?P<developer_id>\w+)/$', 'developer_app.views.view_developer', name='view_developer'),
-    url(r'^developers/(?P<developer_id>\w+)/edit/$', 'developer_app.views.edit_developer', name='edit_developer'),
-    url(r'^developers/(?P<developer_id>\w+)/delete/$', 'developer_app.views.delete_developer', name='delete_developer'),
-
-    url(r'^company/$', 'company_app.views.companies', name='companies'),
-    url(r'^company/new/$', 'company_app.views.new_company', name='new_company'),
-    url(r'^company/(?P<developer_id>\w+)/$', 'company_app.views.view_company', name='view_company'),
-    url(r'^company/(?P<developer_id>\w+)/edit/$', 'company_app.views.edit_company', name='edit_company'),
-    url(r'^company/(?P<developer_id>\w+)/delete/$', 'company_app.views.delete_company', name='delete_company'),
+    # url(r'^developers/$', 'developer_app.views.developers', name='developers'),
+    # url(r'^developers/new/$', 'developer_app.views.new_developer', name='new_developer'),
+    # url(r'^developers/(?P<developer_id>\w+)/$', 'developer_app.views.view_developer', name='view_developer'),
+    # url(r'^developers/(?P<developer_id>\w+)/edit/$', 'developer_app.views.edit_developer', name='edit_developer'),
+    # url(r'^developers/(?P<developer_id>\w+)/delete/$', 'developer_app.views.delete_developer', name='delete_developer'),
+    #
+    # url(r'^company/$', 'company_app.views.companies', name='companies'),
+    # url(r'^company/new/$', 'company_app.views.new_company', name='new_company'),
+    # url(r'^company/(?P<developer_id>\w+)/$', 'company_app.views.view_company', name='view_company'),
+    # url(r'^company/(?P<developer_id>\w+)/edit/$', 'company_app.views.edit_company', name='edit_company'),
+    # url(r'^company/(?P<developer_id>\w+)/delete/$', 'company_app.views.delete_company', name='delete_company'),
 
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^$', 'developer_app.views.index', name='index'),
+    # url(r'^$', 'developer_app.views.index', name='index'),
 
     url(r'^register/developer/$',
         RegistrationView.as_view(success_url='/developer/new/'),
