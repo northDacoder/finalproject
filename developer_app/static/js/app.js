de724881bf8b8b1ad136c326ccff01b8466cadfc
John Ryan <jcryan85@gmail.com>
2014-03-12 21:41:07 -0700

    moved registration to templates directory

--- developer_app/static/js/app.js
+++ developer_app/static/js/app.js
@@ -10,5 +10,19 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
         .when('/developer', { templateUrl: '/static/views/developer.html', controller: 'classController' })
         .when('/company', { templateUrl: '/static/views/company.html', controller: 'classController' })
         .when('/add_developer', { templateUrl: '/static/views/add_developer.html', controller: 'classController' })
-        .when('/delete_developer', { templateUrl: '/static/views/delete_developer.html', controller: 'classController' });
+        .when('/login/activate', { templateUrl: '/static/views/registration/delete_developer.html', controller: 'loginController' })
+        .when('/login/activation_complete', { templateUrl: '/static/views/registration/activation_complete.html', controller: 'loginController' })
+        .when('/login', { templateUrl: '/static/views/registration/login.html', controller: 'loginController' })
+        .when('/login/logout', { templateUrl: '/static/views/registration/logout.html', controller: 'loginController' })
+        .when('/account/password_change_done', { templateUrl: '/static/views/registration/password_change_done.html', controller: 'loginController' })
+        .when('/account/password_change_form', { templateUrl: '/static/views/registration/password_change_form.html', controller: 'loginController' })
+        .when('/account/password_reset_complete', { templateUrl: '/static/views/registration/password_reset_complete.html', controller: 'loginController' })
+        .when('/account/password_reset_confirm', { templateUrl: '/static/views/registration/password_reset_confirm.html', controller: 'loginController' })
+        .when('/account/password_reset_done', { templateUrl: '/static/views/registration/password_reset_done.html', controller: 'loginController' })
+        .when('/account/password_reset_email', { templateUrl: '/static/views/registration/password_reset_email.html', controller: 'loginController' })
+        .when('/account/password_reset_form', { templateUrl: '/static/views/registration/password_reset_form.html', controller: 'loginController' })
+        .when('/registration/closed', { templateUrl: '/static/views/registration/registration_closed.html', controller: 'loginController' })
+        .when('/registration/complete', { templateUrl: '/static/views/registration/registration_closed.html', controller: 'loginController' })
+        .when('/registration/form', { templateUrl: '/static/views/registration/registration_closed.html', controller: 'loginController' })
+
 }]);
