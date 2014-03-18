924f25b121359e12f7e12f859905534ed60a5cdc
John Ryan <jcryan85@gmail.com>
2014-03-18 15:29:42 -0700

    changes to company profile

--- developer_app/static/js/app.js
+++ developer_app/static/js/app.js
@@ -5,6 +5,7 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
         .when('/', { templateUrl: '/static/views/index.html', controller: 'indexController' })
         .when('/developer', { templateUrl: '/static/views/developer.html', controller: 'developerController' })
         .when('/company', { templateUrl: '/static/views/company.html', controller: 'companyController' })
+        .when('/contact', { templateUrl: '/static/views/contact.html' })
         .when('/add_developer', { templateUrl: '/static/views/add_developer.html', controller: 'addDeveloperController' })
         .when('/login/activate', { templateUrl: '/static/views/registration/delete_developer.html', controller: 'loginController' })
         .when('/login/activation_complete', { templateUrl: '/static/views/registration/activation_complete.html', controller: 'loginController' })
--- developer_app/static/js/controllers/addDeveloperController.js
+++ developer_app/static/js/controllers/addDeveloperController.js
@@ -1,3 +1,7 @@
 function addDeveloperController($scope, $http) {
+    console.log($scope.user);
 
-};
+    $http.post('api/v1/developer/', $scope.user).success(function(){
+        alert('Add a Developer?');
+    });
+}
