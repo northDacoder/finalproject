871f22c4a149fbaec9a851a7ad8e11ec75410a08
John Ryan <jcryan85@gmail.com>
2014-03-20 16:17:33 -0700

    updated default values for company models

--- company_app/models.py
+++ company_app/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    phone = models.CharField(max_length=12, blank=True, null=True)
-    description = models.TextField(max_length=6000)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=14, blank=True, null=True)
+    description = models.TextField(max_length=6000, blank=True, null=True)
     posted = models.DateTimeField(auto_now=True)
     website = models.URLField(max_length=1000, blank=True, null=True)
-    github = models.URLField(max_length=1000)
-    email = models.EmailField(null=False)
-    cover = models.ImageField(upload_to="images/company_coverphoto")
-    screenshot = models.ImageField(upload_to="images/company_screenshots")
+    github = models.URLField(max_length=1000, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    cover = models.ImageField(upload_to="images/company_coverphoto", blank=True, null=True)
+    screenshot = models.ImageField(upload_to="images/company_screenshots", blank=True, null=True)
     user = models.ForeignKey(User)
 
     def __unicode__(self):
@@ -31,10 +31,10 @@ class CompanyProject(models.Model):
     project_name = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now=True)
     company = models.ForeignKey(Company)
-    language = models.ForeignKey(Language)
+    language = models.ForeignKey(Language, blank=True, null=True)
     completed = models.BooleanField()
-    project_screenshot = models.ImageField(upload_to="images/developer_screenshots")
-    description = models.TextField(max_length=6000)
+    project_screenshot = models.ImageField(upload_to="images/developer_screenshots", blank=True, null=True)
+    description = models.TextField(max_length=6000, blank=True, null=True)
     accepted_project = models.OneToOneField('developer_app.DeveloperProject', related_name="company_pick", blank=True, null=True)
 
     def __unicode__(self):
