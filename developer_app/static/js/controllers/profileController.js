b2ce87f800125b6bb39cad7daedff44ae95598f9
John Ryan <jcryan85@gmail.com>
2014-03-19 17:55:53 -0700

    updates to user profile

--- developer_app/static/js/controllers/profileController.js
+++
@@ -1,8 +0,0 @@
-function profileController($scope, $http, $routeParams) {
-
-    $http.get('api/v1/myProfile/' + $routeParams.id + '?format=json').success(function(data){
-        console.log(data);
-        $scope.profile = data.objects;
-    });
-
-}
