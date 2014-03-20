9b7b260f624396bf8271fa445a09a576f2abeb4e
John Ryan <jcryan85@gmail.com>
2014-03-20 16:20:00 -0700

    updates to developer_app model

--- developer_app/models.py
+++ developer_app/models.py
@@ -5,18 +5,18 @@ from company_app.models import CompanyProject, Language
 
 class Developer(models.Model):
     name = models.CharField(max_length=100)
-    age = models.CharField(max_length=100)
+    age = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
-    state = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
     phone = models.IntegerField(max_length=10, null=True, blank=False)
     posted = models.DateTimeField(auto_now=True)
     website = models.URLField(max_length=1000, blank=True, null=True)
-    github = models.URLField(max_length=1000)
+    github = models.URLField(max_length=1000, blank=True, null=True)
     languages = models.ManyToManyField(Language)
-    description = models.TextField(max_length=6000)
-    cover = models.ImageField(upload_to="images/developer_coverphoto")
-    screenshot = models.ImageField(upload_to="images/developer_screenshots")
+    description = models.TextField(max_length=6000, blank=True, null=True)
+    cover = models.ImageField(upload_to="images/developer_coverphoto", blank=True, null=True)
+    screenshot = models.ImageField(upload_to="images/developer_screenshots", blank=True, null=True)
     user = models.ForeignKey(User)
 
     def __unicode__(self):
@@ -28,8 +28,8 @@ class DeveloperProject(models.Model):
     created = models.DateTimeField(auto_now=True)
     developer = models.ForeignKey(Developer, related_name="projects")
     company_project = models.ForeignKey(CompanyProject, related_name="developer_projects")
-    project_screenshot = models.ImageField(upload_to="images/developer_screenshots")
-    description = models.TextField(max_length=6000)
+    project_screenshot = models.ImageField(upload_to="images/developer_screenshots", blank=True, null=True)
+    description = models.TextField(max_length=6000, blank=True, null=True)
     completed = models.BooleanField()
 
     def __unicode__(self):
