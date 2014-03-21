a708510450ab408c913f73141cb9e0325a46d315
John Ryan <jcryan85@gmail.com>
2014-03-21 11:39:58 -0700

    added project controller

--- developer_app/static/js/controllers/developerController.js
+++ developer_app/static/js/controllers/developerController.js
@@ -4,6 +4,11 @@ function developerController($scope, $http) {
         console.log($scope.developers);
     });
 
+    $http.get('api/v1/developerproject/?format=json').success(function(data){
+        $scope.projects = data.objects;
+        console.log($scope.projects);
+    })
+
     $scope.addDeveloperProject = function() {
         console.log($scope.user);
         $http.post('api/v1/developers/', $scope.user).success(function(){
