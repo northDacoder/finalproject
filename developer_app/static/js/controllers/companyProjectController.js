2d7e8fe34c330d254248670847a84f2cd4bcae96
John Ryan <jcryan85@gmail.com>
2014-03-21 11:46:51 -0700

    updates to controllers

--- developer_app/static/js/controllers/companyProjectController.js
+++ developer_app/static/js/controllers/companyProjectController.js
@@ -1,16 +1,16 @@
 function companyProjectController($scope, $http) {
 
-    $scope.addCompanyProject = function() {
-        console.log($scope.user);
-        $http.post('api/v1/companyproject/', $scope.user).success(function(){
-            alert('Add a developer project?');
-        });
-    }
-
-    $scope.deleteCompanyProject = function() {
-        console.log($scope.user);
-        $http.delete('api/v1/companyproject/', $scope.user).success(function(){
-            alert('You are trying to delete a developer project!');
-        });
-    }
+//    $scope.addCompanyProject = function() {
+//        console.log($scope.user);
+//        $http.post('api/v1/companyproject/', $scope.user).success(function(){
+//            alert('Add a developer project?');
+//        });
+//    }
+//
+//    $scope.deleteCompanyProject = function() {
+//        console.log($scope.user);
+//        $http.delete('api/v1/companyproject/', $scope.user).success(function(){
+//            alert('You are trying to delete a developer project!');
+//        });
+//    }
 }
