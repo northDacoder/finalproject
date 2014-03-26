1f21ca8440c94f5ad0f3a6b5ddf965a45e4e3fd3
John Ryan <jcryan85@gmail.com>
2014-03-26 14:03:46 -0700

    updates to styles

--- developer_app/static/js/controllers/developerController.js
+++ developer_app/static/js/controllers/developerController.js
@@ -1,18 +1,13 @@
 function developerController($scope, $http) {
+
     $http.get('api/v1/developer/?format=json').success(function(data){
         $scope.developers = data.objects;
         console.log($scope.developers);
     });
 
-    $http.get('api/v1/developerproject/?format=json').success(function(data){
-        $scope.projects = data.objects;
-        console.log($scope.projects);
-    })
-
-//    $scope.addDeveloperProject = function() {
-//        console.log($scope.user);
-//        $http.post('api/v1/developers/', $scope.user).success(function(){
-//            alert('Add a developer project?');
-//        });
-//    }
 }
+
+//    $http.get('api/v1/developerproject/?format=json').success(function(data){
+//        $scope.projects = data.objects;
+//        console.log($scope.projects);
+//    })
