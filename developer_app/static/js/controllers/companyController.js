0b74aa63d56972a3ddc1dbd60a4c80105a65b84a
John Ryan <jcryan85@gmail.com>
2014-04-01 18:21:46 -0400

    updates styles to buttons on developer portal

--- developer_app/static/js/controllers/companyController.js
+++ developer_app/static/js/controllers/companyController.js
@@ -1,6 +1,7 @@
 function companyController($scope, $http) {
 
     $http.get('api/v1/company/?format=json').success(function(data){
+        console.log(data);
         $scope.companies = data.objects;
         console.log($scope.companies);
     });
