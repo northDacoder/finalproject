5e0b31191a361baa6083ad63599302fe6986833c
John Ryan <jcryan85@gmail.com>
2014-03-31 23:37:50 -0400

    cleaned up urls.py for Django Auth package

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -21,20 +21,20 @@ urlpatterns = patterns('',
 
     url(r'^api/', include(v1_api.urls)),
 
+    # Users
     url(r'^$', 'developer_app.views.angular', name="angular"),
-    url(r'^register/user/$', RegistrationView.as_view(success_url='/user/home/'), name='registration_register_developer',),
 
-    #url(r'^accounts/login/', 'users.views.login_page', name='login'),
-    url(r'^accounts/password/change/$', auth_views.password_change_form, name='password_change'),
+    # Django Auth
+    url(r'accounts/', include('registration.backends.default.urls')),
+    url(r'^register/user/$', RegistrationView.as_view(success_url='#/user/home/'), name='registration_register_developer',),
+    url(r'^accounts/password/change/$', auth_views.password_change, name='password_change'),
     url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
-
     url(r'^accounts/password/reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^accounts/password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
 
-    #and now add the registration urls
-    url(r'accounts/', include('registration.backends.default.urls')),
+    # Admin
     url(r'^admin/', include(admin.site.urls)),
 
 )
