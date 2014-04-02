473b9fba75e0ba67a9ea227634c1074fb8493670
John Ryan <jcryan85@gmail.com>
2014-04-02 09:41:57 -0400

    updates to developer controller.js

--- developer_app/static/js/controllers/developerController.js
+++ developer_app/static/js/controllers/developerController.js
@@ -1,16 +1,12 @@
 function developerController($scope, $http) {
 
-    $scope.developers = {};
-    $scope.developers.projects = {};
-
     $http.get('api/v1/developer/?format=json').success(function(data){
         $scope.developers = data.objects;
         console.log($scope.developers);
     });
 
-     $http.get('api/v1/developerproject/?format=json').success(function(data){
-        $scope.projects = data.objects;
-        console.log($scope.projects);
-    });
-
+//    $http.get('api/v1/developerproject/?format=json').success(function(data){
+//        $scope.projects = data.objects;
+//        console.log($scope.projects);
+//    });
 }
